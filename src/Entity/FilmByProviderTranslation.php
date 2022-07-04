3df37c61841c33f80901d53ae3e481cc8f4aa29c
sjay23 <stas.sh23@gmail.com>
2022-07-04 19:59:24 +0300

    add migration

---
+++ src/Entity/FilmByProviderTranslation.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslationInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+
+/**
+ * @ORM\Entity()
+ */
+class FilmByProviderTranslation implements TranslationInterface
+{
+    use TranslationTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=500, unique="true")
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(type="string", length=5000, nullable="true")
+     */
+    private ?string $description;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
