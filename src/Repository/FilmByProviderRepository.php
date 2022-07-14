54cc35e6ed976e27f83fa725d78b34b312360451
sjay23 <stas.sh23@gmail.com>
2022-07-14 13:45:35 +0300

    fix

--- src/Repository/FilmByProviderRepository.php
+++ src/Repository/FilmByProviderRepository.php
@@ -40,49 +40,4 @@ class FilmByProviderRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return FilmByProvider[] Returns an array of FilmByProvider objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('f.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?FilmByProvider
-//    {
-//        return $this->createQueryBuilder('f')
-//            ->andWhere('f.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
-
-
-        public function test(Provider $provider)
-        {
-            /**
-             * Как добавить перевод пример
-             */
-            $filmsProvider = new FilmByProvider();
-            $filmsProvider->translate('en')->setTitle('my title in en1');
-            $filmsProvider->translate('ru')->setTitle('my title in ru');
-            $filmsProvider->translate('en')->setDescription('my content in en1');
-            $filmsProvider->translate('ru')->setDescription('my content in ru');
-            $filmsProvider->setProvider($provider);
-            $filmsProvider->setLink('https://github.com/doctrine-extensions/DoctrineExtensions/blob/main/doc/translatable.md#advanced-examples');
-            $this->_em->persist($filmsProvider);
-            $filmsProvider->mergeNewTranslations();
-            $test = $filmsProvider->translate('ru')->getTitle();
-            dump($test);die();
-            $this->_em->flush();
-        }
 }
