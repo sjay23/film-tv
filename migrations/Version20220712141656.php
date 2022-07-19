92797377a589f909d58b4261b8d6c0649e547c5f
Ivan Zelenskyi <ivan.zelenskyi@jelvix.com>
2022-07-19 17:44:21 +0300

    code reformat

--- migrations/Version20220712141656.php
+++ migrations/Version20220712141656.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220719130117 extends AbstractMigration
+final class Version20220712141656 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20220719130117 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE film_by_provider ADD poster_uploaded SMALLINT DEFAULT 0 NOT NULL');
-        $this->addSql('ALTER TABLE film_by_provider_translation ADD banner_uploaded SMALLINT DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE command_task ADD count_task INT NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE film_by_provider DROP poster_uploaded');
-        $this->addSql('ALTER TABLE film_by_provider_translation DROP banner_uploaded');
+        $this->addSql('ALTER TABLE command_task DROP count_task');
     }
 }
