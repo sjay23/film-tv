f3f29676a803eaef1597c0dea2293b7220cb703c
Ivan Zelenskyi <ivan.zelenskyi@jelvix.com>
2022-09-01 18:03:34 +0300

    add sphinx

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Javer\SphinxBundle\JaverSphinxBundle::class => ['all' => true],
 ];
