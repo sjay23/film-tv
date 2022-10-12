c0de2c36a78dbe91baaf554ce2954d1704dd651d
sjay23 <stas.sh23@gmail.com>
2022-10-12 17:07:56 +0300

    fix

--- tests/GenreTest.php
+++ tests/GenreTest.php
@@ -4,17 +4,11 @@ namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase as SymfonyApiTestCase;
 use App\Entity\Genre;
-
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
 class GenreTest extends SymfonyApiTestCase
 {
-
-
-    protected function setUp(): void
-    {
-        $this->client = static::createClient();
-        $this->userToken = $this->createJwtToken('test@jelvix.com', 'ROLE_SUPER_ADMIN');
-    }
+    //use RefreshDatabaseTrait;
 
     public function testGetCollection(): void
     {
@@ -27,18 +21,4 @@ class GenreTest extends SymfonyApiTestCase
         $this->assertResponseHeaderSame('content-type', 'application/json; charset=utf-8');
         $this->assertMatchesResourceCollectionJsonSchema(Genre::class);
     }
-
-    /**
-     * @param string $username
-     * @param string $role
-     * @return string
-     */
-    protected function createJwtToken(string $username, string $role): string
-    {
-        $data = array('username' => $username, 'roles' => [$role]);
-        return $this->client->getContainer()
-            ->get('lexik_jwt_authentication.encoder')
-            ->encode($data);
-    }
-
 }
