255e1b0596db7e63e7600ac4a1553ba9e7921c05
Ivan Zelenskyi <ivan.zelenskyi@jelvix.com>
2022-11-02 12:05:42 +0200

    add tests

--- tests/ParseFilmTest.php
+++ tests/ParseFilmTest.php
@@ -11,13 +11,52 @@ class ParseFilmTest extends TestUnitMain
         return new FilmFieldService($this->validator);
     }
 
-    public function testParseFilm()
+    public function testParseFilmAge()
     {
         $age = $this->getService()->parseAge($this->getCrawlerByLink());
-        $filmId = $this->getService()->parseFilmId($this->link);
-
-        $this->assertEquals("7585835", $filmId);
         $this->assertEquals("18+", $age);
+    }
+
+    public function testParseFilmId()
+    {
+        $filmId = $this->getService()->parseFilmId($this->link);
+        $this->assertEquals("7585835", $filmId);
+    }
+
+    public function testParseFilmDuration()
+    {
+        $duration = $this->getService()->parseDuration($this->getCrawlerByLink());
+        $this->assertEquals("105", $duration);
+    }
+
+    public function testParseFilmYear()
+    {
+        $year = $this->getService()->parseAge($this->getCrawlerByLink());
+        $this->assertEquals("18+", $year);
+    }
+
+    public function testParseFilmAudio()
+    {
+        $audio = $this->getService()->parseAudio($this->getCrawlerByLink());
+        $this->assertEquals(" English ", ($audio[1])->getName());
+    }
+
+    public function testParseFilmGenre()
+    {
+        $genre = $this->getService()->parseGenre($this->getCrawlerByLink());
+        $this->assertEquals("Thriller", ($genre[0])->getName());
+    }
+
+    public function testParseFilmCountry()
+    {
+        $country = $this->getService()->parseCountry($this->getCrawlerByLink());
+        $this->assertEquals("USA", ($country[0])->getName());
+    }
+
+    public function testParseFilmRating()
+    {
+        $rating = $this->getService()->parseRating($this->getCrawlerByLink());
+        $this->assertEquals("5.2", $rating);
     }
 
 }
